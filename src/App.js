b45ff9e23d33655f14333fdbe742a30d98a19370
rafaeelnunes <rafaelnfsq@gmail.com>
2022-02-08 20:43:05 -0300

    initial config

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+}
