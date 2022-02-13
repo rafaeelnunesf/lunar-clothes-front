d7569ff99ba2b0587eb430a34d1c248cad4bc3c1
rafaeelnunes <rafaelnfsq@gmail.com>
2022-02-13 11:11:58 -0300

    refactor: Change the Header structure to be reused

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 import back from "../../assets/back.svg";
 import search from "../../assets/search.svg";
-export default function Header() {
+export default function Header({ page, hasSearch }) {
   return (
     <ContainerHeader>
       <div>
         <img src={back} alt="" />
-        <h1>Lunar Clothes</h1>
-        <img src={search} alt="" />
+        <h1>{page}</h1>
+        {hasSearch && <img src={search} alt="" />}
       </div>
     </ContainerHeader>
   );
@@ -34,6 +34,10 @@ const ContainerHeader = styled.div`
 
     text-align: center;
     color: #222222;
+
+    position: absolute;
+    left: 0;
+    right: 0;
   }
   box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.12);
   margin-bottom: 16px;
