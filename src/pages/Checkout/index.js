c23f7e8fa304f87bd2a42bb0605236a3027f6bbc
rafaeelnunes <rafaelnfsq@gmail.com>
2022-02-14 03:03:22 -0300

    feat: add feature add address and payment method

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -8,16 +8,24 @@ import {
   Values,
   SubmitButton,
 } from "../../components/Checkout/index.js";
+import AddAddress from "./AddAddress";
+import AddPaymentMethod from "./AddPaymentMethod";
+import { useState } from "react";
 
 export default function Checkout() {
+  const [addressActive, setAddressActive] = useState(false);
+  const [paymentActive, setPaymentActive] = useState(false);
+
   return (
     <Container>
       <Header page={"Checkout"} hasSearch={false}></Header>
-      <Address />
-      <Payment />
+      <Address>{setAddressActive}</Address>
+      <Payment>{setPaymentActive}</Payment>
       <Delivery />
       <Values />
       <SubmitButton>SUBMIT ORDER</SubmitButton>
+      {addressActive && <AddAddress>{setAddressActive}</AddAddress>}
+      {paymentActive && <AddPaymentMethod>{setPaymentActive}</AddPaymentMethod>}
     </Container>
   );
 }
